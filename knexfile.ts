8fb12600db2652257362c9051015ded40f6e9857
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-04-01 21:05:54 -0300

    Adicionando produto no banco

--- knexfile.ts
+++ knexfile.ts
@@ -1,21 +1,21 @@
 export default {
-  client: "sqlite3",
+  client: "sqlite3",  // Define o SQLite3 como banco de dados
   connection: {
-    filename: "./src/database/database.db",
+    filename: "./src/database/database.db", // Caminho do arquivo SQLite
   },
   pool: {
     afterCreate: (connection: any, done: any) => {
-      connection.run("PRAGMA foreign_keys = ON")
-      done()
+      connection.run("PRAGMA foreign_keys = ON"); // Ativa chaves estrangeiras
+      done();
     },
   },
-  useNullAsDefault: true,
+  useNullAsDefault: true, // Garante que colunas sem valores padrão recebam NULL
   migrations: {
-    extensions: "ts",
-    directory: "./src/database/migrations",
+    extensions: "ts", // Especifica que as migrações são arquivos TypeScript
+    directory: "./src/database/migrations", // Diretório das migrações
   },
   seeds: {
-    extensions: "ts",
-    directory: "./src/database/seeds",
-  }
-}
+    extensions: "ts", // Especifica que os seeds são arquivos TypeScript
+    directory: "./src/database/seeds", // Diretório dos seeds
+  },
+};
