48c9323c97b8d82488c73ef6647532b2a8fce821
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-04-05 15:27:17 -0300

    Tratando Dados do Pedido

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
 import { tablesSessionsRoutes } from "./tables-sessions-routes";
+import { ordersRoutes } from "./orders-routes";
 import { productsRoutes } from "./products-routes";
 import { tablesRoutes } from "./tables-routes";
 
 const routes = Router();
 
 routes.use("/tables-sessions", tablesSessionsRoutes);
+routes.use("/orders", ordersRoutes);
 routes.use("/products", productsRoutes);
 routes.use("/tables", tablesRoutes);
 
